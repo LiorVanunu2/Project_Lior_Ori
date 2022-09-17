50bf85469e237aa0822b9e92e856e36d7c205a4d
oriiiiiiiiiiiii <ori123ori31@gmail.com>
2022-09-17 22:00:23 +0300

    Change file

--- Screen.py
+++ Screen.py
@@ -1,12 +1,14 @@
 import pygame
+import time
 
 # Set the size of each cell in the screen
 size = 20
 black_color = (10, 10, 10)
 grid_color = (40, 40, 40)
+green_color = (61, 145, 64)
 
 
-def fill_colors(matrix, screen, size):
+def create_background_matrix(matrix, screen, size):
     num_rows = len(matrix)
     num_cols = len(matrix[0])
     # run over all the cells in the current matrix
@@ -18,6 +20,11 @@ def fill_colors(matrix, screen, size):
     pygame.display.update()
 
 
+def set_regular_background_color(color):
+    screen.fill(color)
+    pygame.display.flip()
+
+
 pygame.init()
 # Set the screen size
 num_cols, num_rows = 50, 25
@@ -27,10 +34,16 @@ pygame.display.flip()
 pygame.display.update()
 background_matrix = [[0 for x in range(num_cols)] for y in
                      range(num_rows)]
-fill_colors(background_matrix, screen, size)
 while True:
     # Wait for events
     for event in pygame.event.get():
+        set_regular_background_color(green_color)
         # if user wants to QUIT, close pygame
         if event.type == pygame.QUIT:
             pygame.quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                create_background_matrix(background_matrix, screen, size)
+
+
+
