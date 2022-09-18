f8345b3e78f7df89b5f8b7a0af3f33dc1fb3ef29
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-18 14:06:57 +0300

    update

--- Soldier.py
+++ Soldier.py
@@ -2,11 +2,8 @@ import Screen
 import pygame
 import consts
 
-IMAGE = 'grass.png'
-img = pygame.image.load(IMAGE)
-Screen.blit(img, (0, 0))
-pygame.display.flip()
 
-soldier_img = pygame.image.load("C:\\Users\\pc\\Desktop\\lior_ori\\Project_Lior_Ori\\Pictures\\soldier.png").convert()
-Screen.screen.blit(soldier_img, [5, 8])
-pygame.display.flip()
+# def draw_soldier():
+#     soldier_image = pygame.image.load(os.path.join("picturs", consts.SOLDIER_IMAGE))
+#     soldier = pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
+#     screen.blit(soldier, consts.BEGIN_POINT)
