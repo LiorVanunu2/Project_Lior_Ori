d053c3493ae0ad3a48396e38b2be50dcb05d802b
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-19 16:23:01 +0300

    add movement soldier

--- Soldier.py
+++ Soldier.py
@@ -3,7 +3,21 @@ import pygame
 import consts
 
 
-# def draw_soldier():
-#     soldier_image = pygame.image.load(os.path.join("picturs", consts.SOLDIER_IMAGE))
-#     soldier = pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
-#     screen.blit(soldier, consts.BEGIN_POINT)
+# The moveRight() method takes two arguments. The first one is implicit and is called self.
+# It refers to the current object. The second one is called pixels and refers to the number of pixels we will use to move the car.
+def moveRight(soldier):
+    soldier.rect.x += consts.SIZE
+
+    # self.rect.x += consts.size
+
+
+def moveLeft(self):
+    self.rect.x -= consts.SIZE
+
+
+def moveUp(self):
+    self.rect.y += consts.SIZE
+
+
+def moveDown(self):
+    self.rect.y -= consts.SIZE
