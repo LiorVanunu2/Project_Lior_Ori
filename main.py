190f9c376143f8bc54a69d694cfe55e7058b0551
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-19 09:34:06 +0300

    add main

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import Screen
+import pygame
+import consts
+import time
 
-Screen.init_screen()
+def main():
+    run_game = True
+    pygame.init()
+    Screen.init_screen()
+    while run_game:
+        for event in pygame.event.get():
+            # if user wants to QUIT, close pygame
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    Screen.draw_grid_background()
+                    time.sleep(consts.SHOW_MATRIX)
+                else: pass
+        Screen.init_screen()
+
+
+
+if __name__ == '__main__':
+    main()
