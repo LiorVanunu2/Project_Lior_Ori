1adbd58830cd79f14d8f1a6905a6e451dbd8ed00
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-19 15:33:49 +0300

    add

--- Screen.py
+++ Screen.py
@@ -3,20 +3,75 @@ import time
 import os
 import Soldier
 import consts
+import random
+import MineField
 
 screen = pygame.display.set_mode((consts.NUM_COL * consts.SIZE, consts.NUM_ROW * consts.SIZE))
+dic_list_xy = {}
+
+
 def put_flag():
     flag_image = pygame.image.load(os.path.join("Pictures", consts.FLAG_IMAGE))
     flag = pygame.transform.scale(flag_image, consts.FLAG_SIZE)
     screen.blit(flag, consts.FLAG_START)
 
+
 def put_soldier():
-    soldier_image=pygame.image.load(os.path.join("Pictures", consts.SOLDIER_IMAGE))
-    soldier=pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
+    soldier_image = pygame.image.load(os.path.join("Pictures", consts.SOLDIER_IMAGE))
+    soldier = pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
     screen.blit(soldier, consts.SOLDIER_START)
 
-# Set the size of each cell in the screen
 
+# Set the size of each cell in the screen
+def place_x_grass():
+    """
+
+    :return: a list of random x places for grass
+    """
+    list_x = []
+    for i in range(20):
+        ran_num = random.randint(0, consts.NUM_COL * 20 - 1 - consts.GRASS_SIZE[0])
+        list_x.append(ran_num)
+    dic_list_xy['list_x'] = list_x
+
+
+def place_y_grass():
+    """
+
+    :return: a list of random y places for grass
+    """
+    list_Y = []
+    for i in range(20):
+        ran_num = random.randint(0, consts.NUM_ROW * 20 - 1 - consts.GRASS_SIZE[1])
+        list_Y.append(ran_num)
+    dic_list_xy['list_y'] = list_Y
+
+
+def put_grass():
+    """
+    put the grass in random place on screen
+    :return: null
+    """
+    list_x = dic_list_xy['list_x']
+    list_y = dic_list_xy['list_y']
+
+    for i in range(20):
+        grass_image = pygame.image.load(os.path.join("Pictures", consts.GRASS_IMAGE))
+        grass = pygame.transform.scale(grass_image, consts.GRASS_SIZE)
+        screen.blit(grass, (list_x[i], list_y[i]))
+
+
+def put_mine():
+    """
+    take the mine picture and put in the screen 20 times in random place.
+    :return: null
+    """
+    list_x = MineField.dic_list_xy_mine['list_x']
+    list_y = MineField.dic_list_xy_mine['list_y']
+    for i in range(20):
+        mine_image = pygame.image.load(os.path.join("Pictures", consts.MINE_IMAGE))
+        mine = pygame.transform.scale(mine_image, consts.MINE_SIZE)
+        screen.blit(mine, (list_x[i], list_y[i]))
 
 
 def create_background_matrix(matrix, screen, size):
@@ -28,23 +83,20 @@ def create_background_matrix(matrix, screen, size):
             color = consts.BLACK_COLOR
             pygame.draw.rect(screen, color,
                              (j * size, i * size, size - 1, size - 1))
-    # להוסיף קריאה לפונקציה ליצירת מוקשים וגם פונקציה להראות את החייל
+    #         להוסיף קריאה לפונקציה ליצירת מוקשים וגם פונקציה להראות את החייל
+    pygame.display.update()
     put_flag()
     put_soldier()
+    put_mine()
     pygame.display.update()
+
 
 def draw_grid_background():
     pygame.display.flip()
     pygame.display.update()
     # Create zeros matrix
     matrix = [[0 for x in range(consts.NUM_COL)] for y in range(consts.NUM_ROW)]
-    create_background_matrix(matrix, screen, consts.SIZE)
-
-
-
-# def set_regular_background_color():
-#     screen.fill(consts.GREEN_COLOR)
-#     pygame.display.flip()
+    MineField.create_background_matrix(matrix, screen, consts.SIZE)
 
 
 pygame.init()
@@ -55,23 +107,11 @@ pygame.display.flip()
 pygame.display.update()
 background_matrix = [[0 for x in range(num_cols)] for y in
                      range(num_rows)]
-# while True:
-#     # Wait for events
-#     for event in pygame.event.get():
-#         set_regular_background_color(green_color)
-#         # if user wants to QUIT, close pygame
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#         elif event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_RETURN:
-#                 create_background_matrix(background_matrix, screen, size)
 
-def init_screen ():
+
+def init_screen():
     screen.fill(consts.GREEN_COLOR)
     put_flag()
     put_soldier()
+    put_grass()
     pygame.display.update()
-
-
-
-
