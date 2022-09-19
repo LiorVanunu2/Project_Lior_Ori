295e6fdc198007cef95a492a2b84ebd1bde99149
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-19 15:32:47 +0300

    add

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import Screen
 import pygame
 import consts
 import time
+import MineField
 
 def main():
     run_game = True
     pygame.init()
+    Screen.place_x_grass()
+    Screen.place_y_grass()
+    MineField.mine_list_x()
+    MineField.mine_list_y()
     Screen.init_screen()
+
     while run_game:
         for event in pygame.event.get():
             # if user wants to QUIT, close pygame
@@ -16,8 +22,11 @@ def main():
                 if event.key == pygame.K_RETURN:
                     Screen.draw_grid_background()
                     time.sleep(consts.SHOW_MATRIX)
+                    # time.sleep(10)
                 else: pass
         Screen.init_screen()
+
+
 
 
 
