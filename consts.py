fa4782d4cdf658ad6c135e8c3025e9e036e89294
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-19 16:23:49 +0300

    add another const

--- consts.py
+++ consts.py
@@ -1,3 +1,4 @@
+
 import pygame
 import Screen
 
@@ -7,18 +8,19 @@ GRID_COLOR = (40, 40, 40)
 GREEN_COLOR = (61, 145, 64)
 NUM_COL = 50
 NUM_ROW = 25
-SHOW_MATRIX=1
+SHOW_MATRIX = 1
 
 SOLDIER_IMAGE = 'soldier.png'
 SOLDIER_START = [0, 0]
 SOLDIER_SIZE = (40, 80)
+SOLDIER_STEP = 1
 
 FLAG_IMAGE = 'flag.png'
-FLAG_START = ((NUM_COL-4)*SIZE, (NUM_ROW-4)*SIZE)
+FLAG_START = ((NUM_COL - 4) * SIZE, (NUM_ROW - 4) * SIZE)
 FLAG_SIZE = (80, 80)
 
 MINE_IMAGE = 'mine.png'
-MINE_SIZE = (20, 60)
+MINE_SIZE = (60, 20)
 
 GRASS_IMAGE = 'grass.png'
 GRASS_SIZE = (20, 60)
