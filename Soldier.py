dce807b489748fcd084ff95340fbb628f2ca2b5e
oriiiiiiiiiiiii <ori123ori31@gmail.com>
2022-09-20 18:19:27 +0300

    Add file

--- Soldier.py
+++ Soldier.py
@@ -7,7 +7,7 @@ import time
 
 def create_soldier():
     soldier_image = pygame.image.load(
-        os.path.join("Pictures", consts.SOLDIER_IMAGE))
+            os.path.join("Pictures", consts.SOLDIER_IMAGE))
     soldier = pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
     return soldier
 
@@ -15,18 +15,37 @@ def create_soldier():
 def soldier_movement(keys_pressed, soldier_rect):
     if keys_pressed[
         pygame.K_LEFT] and 0 < soldier_rect.x * consts.SOLDIER_STEP < consts.SCREEN_WIDTH:
-        soldier_rect.x -= 1
+        soldier_rect.x -= consts.SOLDIER_STEP * consts.SIZE
     if keys_pressed[
-        pygame.K_RIGHT] and 0 < soldier_rect.x * consts.SOLDIER_STEP + consts.SOLDIER_STEP * 2 < consts.SCREEN_WIDTH:
-        soldier_rect.x += 1
+        pygame.K_RIGHT] and 0 < soldier_rect.x * consts.SOLDIER_STEP + consts.SIZE * 2 < consts.SCREEN_WIDTH:
+        soldier_rect.x += consts.SOLDIER_STEP * consts.SIZE
     if keys_pressed[
         pygame.K_UP] and 0 < soldier_rect.y * consts.SOLDIER_STEP < consts.SCREEN_HEIGHT:
-        soldier_rect.y -= 1
+        soldier_rect.y -= consts.SOLDIER_STEP * consts.SIZE
     if keys_pressed[
-        pygame.K_DOWN] and 0 < soldier_rect.y * consts.SOLDIER_STEP + consts.SOLDIER_STEP * 4 < consts.SCREEN_HEIGHT:
-        soldier_rect.y += 1
+        pygame.K_DOWN] and 0 < soldier_rect.y * consts.SOLDIER_STEP + consts.SIZE * 4 < consts.SCREEN_HEIGHT:
+        soldier_rect.y += consts.SOLDIER_STEP * consts.SIZE
+    Screen.screen.blit(create_soldier(),
+                       (soldier_rect.x, soldier_rect.y))
+    pygame.display.flip()
+    pygame.display.update()
 
-    return soldier_rect
 
 # The moveRight() method takes two arguments. The first one is implicit and is called self.
 # It refers to the current object. The second one is called pixels and refers to the number of pixels we will use to move the car.
+def show_soldier(soldier, soldier_rect):
+    Screen.screen.blit(soldier,
+                       (soldier_rect.x, soldier_rect.y))
+    pygame.display.flip()
+    pygame.display.update()
+
+
+def calculate_legs_index(soldier, soldier_rect):
+    legs_index_list = []
+    legs_list = []
+    for num in range(2):
+        j = soldier_rect.x / consts.SIZE
+        i = soldier_rect.y / consts.SIZE
+        legs_list.append(j)
+
+    pass
