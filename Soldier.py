e94cc37edbaf8c1fe89f932a3c793fa6380a1374
LiorVanunu2 <liorvan2110@gmail.com>
2022-09-20 08:12:39 +0300

    add some

--- Soldier.py
+++ Soldier.py
@@ -7,7 +7,7 @@ import time
 
 def create_soldier():
     soldier_image = pygame.image.load(
-            os.path.join("Pictures", consts.SOLDIER_IMAGE))
+        os.path.join("Pictures", consts.SOLDIER_IMAGE))
     soldier = pygame.transform.scale(soldier_image, consts.SOLDIER_SIZE)
     return soldier
 
@@ -25,6 +25,8 @@ def soldier_movement(keys_pressed, soldier_rect):
     if keys_pressed[
         pygame.K_DOWN] and 0 < soldier_rect.y * consts.SOLDIER_STEP + consts.SOLDIER_STEP * 4 < consts.SCREEN_HEIGHT:
         soldier_rect.y += 1
+
+    return soldier_rect
 
 # The moveRight() method takes two arguments. The first one is implicit and is called self.
 # It refers to the current object. The second one is called pixels and refers to the number of pixels we will use to move the car.
